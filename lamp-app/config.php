f7e1355415335fbf9189e5aaac6775e99b55fd1e
RAMESH KUMAR VERMA <120879499+RAMESHKUMARVERMAGITHUB@users.noreply.github.com>
2024-01-07 21:45:42 +0530

    Update config.php

--- lamp-app/config.php
+++ lamp-app/config.php
@@ -1,10 +1,10 @@
 <?php
 // Database connection parameters
-$db_hostname = "3.110.84.27";
+$db_hostname = "192.168.1.2";
 $db_port     = 3306;
 $db_database = "simple_lamp";
 $db_username = "root";
-$db_password = "ramesh";
+$db_password = "shihab";
 
 // Image upload options
 $storage_option = "hd";
